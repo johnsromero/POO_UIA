be273db0d8bb71f271fee5c0c25bd17dac5c2b9c
John Romero <johns.7@icloud.com>
2016-01-25 01:21:53 -0600

    Estructuras del respositorio

---
+++ practicas/P01/Numero.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Numero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numero;
+        System.out.println("Introduce un número entero:");
+        numero = sc.nextInt();
+        System.out.println("Número introducido: " + numero);
+        System.out.println("Doble de " + numero + " -> "+ 2*numero);
+        System.out.println("Triple de " + numero + " -> "+ 3*numero);
+        
+    }
+}
